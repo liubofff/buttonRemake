680bc22524e7711973d3688ea02e87010032fe79
liubofff <95593546+liubofff@users.noreply.github.com>
2021-12-06 02:24:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
         var $form = $('#form386649083.t-form.js-form-proccess.t-form_inputs-total_3')
         /* $form - jquery объект ссылающийс€ на форму */
             let orderId = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000
-            var btn = $('<button type="button" class="VD_BUTTON">Нажми</button>').on('click', function(){
+            var btn = $('<button type="button" class="VD_BUTTON">Оформить в рассрочку</button>').on('click', function(){
                 vd.create({
                     partnerCode: 'S0088825',
                     partnerProductCode: '0-0-3;0-0-6;0-0-9;0-0-12',
