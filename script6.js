68ea8c65ba353db1447414644d6ade253ffad3ed
liubofff <95593546+liubofff@users.noreply.github.com>
2021-12-06 15:34:45 +0300

    Update and rename script5.js to script6.js

--- script6.js
+++ script6.js
@@ -4,9 +4,9 @@ jQuery(document).ready(function() {
         let orderId = Math.floor(Math.random() * (99999999 - 10000000 + 1)) + 10000000;
         let myClick = function(){
             vd.create({
-                partnerCode: orderId,
+                partnerCode: 'S0088825',
                 partnerProductCode: '0-0-6;0-0-10;0-0-12;0-0-24',
-                orderNum: '00012345',
+                orderNum: orderId,
                 delivery: 1,
                 items: tcart.products.map(function(item) { return {name: item.name, price: item.price, quantity: item.quantity}})
             });
